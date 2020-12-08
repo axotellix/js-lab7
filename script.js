256e6d1b6536a7dada987db66a10cfee146d1f97
axotellix <RSAndrey@list.ru>
2020-12-08 10:44:02 +0300

    Выполнено задание 4

--- script.js
+++ script.js
@@ -45,3 +45,13 @@ console.log(`${c} C\u{00B0} = ${c * 1.8 + 32} F\u{00B0}`);
 console.log(`${f} F\u{00B0} = ${(f - 32) / 1.8} C\u{00B0}`);
 
 console.groupEnd();
+
+
+/* [ TASK 4 > Check if a year is leap ] */
+console.group('\n\nЗадание 4. Проверка високосности года \n\n');
+
+let y = prompt('Задание 4. Укажите год:');
+let res = new Date(2020, 1+1, 0).getDate() === 29 ? true : false;
+alert(`Год ${y}: ${res}`);
+
+console.groupEnd();
