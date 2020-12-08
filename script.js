e81f22b140026ef0769800c41a97888c44b58909
axotellix <RSAndrey@list.ru>
2020-12-08 10:41:32 +0300

    Выполнено задание 2

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ console.log('number + null = ', typeof (num + $null));		// = number
 console.log('boolean + null = ', typeof (bool + $null));	// = number
 
 console.groupEnd();
+
+
+/* [ TASK 2 > Solving rectangle ] */
+const a = 15;		// a-side
+const b = 5;		// b-side
+let Area, Per;		// Area & Perimeter
+
+console.group('\n\nЗадание 2. Решение прямоугольника');
+
+console.log(`a = ${a}, b = ${b}`);
+console.log(`Площадь = ${a * b}, Периметр = ${(a + b) * 2}`);
+console.log(`Отношение площади к периметру = ${a * b / ((a + b) * 2)}`);
+
+console.groupEnd();
